52405ac2324af348bb9bbfcfb867c992bae5a645
topi0247 <karubona.ra247@gmail.com>
2024-10-10 23:04:12 +0900

    fix:query

--- src/get-item-id.sh
+++ src/get-item-id.sh
@@ -4,22 +4,14 @@ set -e
 REPOSITORY_NAME=$(echo "$REPOSITORY" | cut -d '/' -f2)
 
 if [[ "$EVENT_NAME" == "issues" ]]; then
-  ITEM_ID=$(gh api graphql -f query='
-  query($name: String!) {
-    repository(owner: "$OWNER", name: $name) {
-      $QUERY_TARGET {
-        projectItems(first: 1) {
-          nodes {
-            id
-          }
-        }
-      }
-    }
-  }' -f name="$REPOSITORY_NAME" --jq '.data.repository.issue.projectItems.nodes[0].id')
+  QUERY_TARGET="issue(number: \"$NUMBER\")"
 else
-  ITEM_ID=$(gh api graphql -f query='
-  query($name: String!) {
-    repository(owner: "$OWNER", name: $name) {
+  QUERY_TARGET="pull_request(number: \"$NUMBER\")"
+fi
+
+QUERY="
+  query {
+    repository(owner: \"$OWNER\", name: \"$REPOSITORY_NAME\") {
       $QUERY_TARGET {
         projectItems(first: 1) {
           nodes {
@@ -28,7 +20,13 @@ else
         }
       }
     }
-  }' -f name="$REPOSITORY_NAME" --jq '.data.repository.pull_request.projectItems.nodes[0].id')
+  }
+"
+
+if [[ "$EVENT_NAME" == "issues" ]]; then
+  ITEM_ID=$(gh api graphql -f query="$QUERY" -f name="$REPOSITORY_NAME" --jq '.data.repository.issue.projectItems.nodes[0].id')
+else
+  ITEM_ID=$(gh api graphql -f query="$QUERY" -f name="$REPOSITORY_NAME" --jq '.data.repository.pull_request.projectItems.nodes[0].id')
 fi
 
 echo "ITEM_ID=$ITEM_ID" >> $GITHUB_ENV
